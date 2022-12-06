88daf92a0d740dccc08f4ef6e11b77b49499297f
VictoriaSCorreia <104169748+VictoriaSCorreia@users.noreply.github.com>
2022-12-06 17:53:56 -0300

    Create ex074.py

---
+++ pythonexercicios/ex074.py
@@ -0,0 +1,15 @@
+# TUPLAS, MENOR E MAIOR VALORES
+
+from random import randint
+menor = 11
+maior = 0
+tupla = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+print(f'Os valores sorteados foram: {tupla}')
+
+for c in tupla:
+    if c > maior:
+        maior = c
+    if c < menor:
+        menor = c
+print(f'O maior valor sorteado foi: {maior}')
+print(f'O menor valor sorteado foi: {menor}')
