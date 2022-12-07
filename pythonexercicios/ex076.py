f543adbba22898ddf442820c265d545b89f01d0a
VictoriaSCorreia <104169748+VictoriaSCorreia@users.noreply.github.com>
2022-12-07 16:30:05 -0300

    Update ex076.py

--- pythonexercicios/ex076.py
+++ pythonexercicios/ex076.py
@@ -4,6 +4,8 @@ produtos = ('Caneta', 2.35, 'Lápis', 2.00, 'Borracha', 2.25, 'Caderno', 25.99, 
 
 print(f'\n{"="*50:>35}\n{"LISTAGEM DE PREÇOS":>34}\n{"="*50:>35}')
 print(f'{"-"*50}')
+
 for c in range(0, len(produtos)-1, 2):
-    print(f'{produtos[c]}.............R$ {produtos[c+1]:.2f}')
+    print(f'{produtos[c]}', end="")
+    print(f'{"."*(40 - len(produtos[c]))}R$  {produtos[c+1]:.2f}')
 print(f'{"-"*50}\n')
